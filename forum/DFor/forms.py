6b75427f90025e895d5a5264451ff692b9e669d9
ZiomekH <ziomekh@gmail.com>
2012-08-02 22:15:10 +0200

    Działa modyfikacja profilu, bez zmieny avatara

--- forum/DFor/forms.py
+++ forum/DFor/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from models import *
 
+
+class UzytkownikForm(ModelForm):
+    class Meta:
+	model = Uzytkownik
+	
 class PostForm(ModelForm):
   class Meta:
     model = Post
-
--- forum/DFor/views.py
+++ forum/DFor/views.py
@@ -14,38 +14,53 @@ def sprZalogowania(request):
 def sprUzytkownika(request, czyZalogowany):
     if (czyZalogowany):
 	return Uzytkownik.objects.get(id=request.user.id)
-    return '';
+    return ''
     
 def widokFora(request):
-    czyZalogowany = sprZalogowania(request);
-    uzytkownik = sprUzytkownika(request, czyZalogowany);
+    czyZalogowany = sprZalogowania(request)
+    uzytkownik = sprUzytkownika(request, czyZalogowany)
     return render_to_response("fora.html", {'fora': Forum.objects.all(), 'czyZalogowany': czyZalogowany, 'uzytkownik': uzytkownik})
 
 def widokTemat(request, id):
-    czyZalogowany = sprZalogowania(request);
-    uzytkownik = sprUzytkownika(request, czyZalogowany);
+    czyZalogowany = sprZalogowania(request)
+    uzytkownik = sprUzytkownika(request, czyZalogowany)
     return render_to_response("temat.html", {'temat': Temat.objects.get(id=id), 'media': settings.MEDIA_URL, 'czyZalogowany': czyZalogowany, 'uzytkownik': uzytkownik})
     
 def widokUzytkownicy(request):
-    czyZalogowany = sprZalogowania(request);
-    uzytkownik = sprUzytkownika(request, czyZalogowany);
+    czyZalogowany = sprZalogowania(request)
+    uzytkownik = sprUzytkownika(request, czyZalogowany)
     if (czyZalogowany == False):
       return HttpResponseRedirect('/dfor/zaloguj/?next=/dfor/uzytkownicy')
     return render_to_response("uzytkownicy.html", {'uzytkownicy': Uzytkownik.objects.all(), 'czyZalogowany': czyZalogowany, 'uzytkownik': uzytkownik})
 
 def widokUzytkownik(request, id):
-    czyZalogowany = sprZalogowania(request);
-    uzytkownik = sprUzytkownika(request, czyZalogowany);
+    czyZalogowany = sprZalogowania(request)
+    uzytkownik = sprUzytkownika(request, czyZalogowany)
     if (czyZalogowany == False):
       return HttpResponseRedirect('/dfor/zaloguj/?next=/dfor/uzytkownik/' + id)
     return render_to_response("uzytkownik.html", {'uzytk': Uzytkownik.objects.get(id=id), 'media': settings.MEDIA_URL, 'czyZalogowany': czyZalogowany, 'uzytkownik': uzytkownik})
 
 def widokProfil(request):
-    czyZalogowany = sprZalogowania(request);
-    uzytkownik = sprUzytkownika(request, czyZalogowany);
+    czyZalogowany = sprZalogowania(request)
+    uzytkownik = sprUzytkownika(request, czyZalogowany)
     if (czyZalogowany == False):
       return HttpResponseRedirect('/dfor/zaloguj/?next=/dfor/profil')
-    return render_to_response("profil.html", {'media': settings.MEDIA_URL, 'czyZalogowany': czyZalogowany, 'uzytkownik': uzytkownik})
+    dane = {}
+    dane.update(csrf(request))
+    if request.method == "POST":
+	form = UzytkownikForm(request.POST, instance=uzytkownik)
+	if form.is_valid():
+	    form.save()
+	return HttpResponseRedirect('/dfor')
+    else:
+	form = UzytkownikForm(instance=uzytkownik)
+	
+    dane['form'] = form
+    dane['media'] = settings.MEDIA_URL
+    dane['czyZalogowany'] = czyZalogowany
+    dane['uzytkownik'] = uzytkownik
+    
+    return render_to_response('profil.html', dane)
     
 def widokWyloguj(request):
     logout(request)
